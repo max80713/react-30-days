ce904cb524b93a912061d2074a90949a82adf7c7
Max Hsu <max80713@hotmail.com>
2018-10-02 22:22:33 +0800

    Add day one example code

---
+++ 1/index.js
@@ -0,0 +1,14 @@
+let index = 0;
+
+
+
+setInterval(() => {
+  index += 1;
+  const li = React.createElement('li', {}, '第一項'); 
+  const li2 = React.createElement('li', {}, '第二項'); 
+  const li3 = React.createElement('li', {}, index); 
+
+  const ul = React.createElement('ul', {}, li, li2, li3);
+
+  ReactDOM.render(ul, document.getElementById('root'));
+}, 1000);
