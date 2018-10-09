023e5285a482f3b7979b13f545c8014022e2c9c1
Max Hsu <max80713@hotmail.com>
2018-10-09 23:37:56 +0800

    Add day eight example code

--- my-app/src/MyButton.js
+++ my-app/src/MyButton.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './Button.css';
 
 class MyButton extends React.Component {
+  spanRef = React.createRef()
+
   handleClick = () => {
-    window.alert(this.props.text);
+    const number = Number(this.spanRef.current.textContent);
+
+    this.spanRef.current.textContent = number + 1;
   }
 
   render() {
     return (
-      <button onClick={this.handleClick}>{this.props.text}</button>
+      <div>
+        <button onClick={this.handleClick}>{this.props.text}</button>
+        <p>被點擊了<span ref={this.spanRef}>0</span>次</p>
+      </div>
     )
   }
 }
