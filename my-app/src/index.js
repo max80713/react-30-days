1602375365f42a302f45be9a349f85cff19f768b
Max Hsu <max80713@hotmail.com>
2018-10-30 22:05:48 +0800

    Add day twenty-ninth example code

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Form from './Form';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li><Link to="/app">App</Link></li>
+          <li><Link to="/form">Form</Link></li>
+        </ul>
+        <Route path="/app" component={App} />
+        <Route path="/form" component={Form} />
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
